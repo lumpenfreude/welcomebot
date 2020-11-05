3eb544ad899581c5c4079d6aa05679d38395f634
lumpenfreude <lumpenfreude@gmail.com>
2020-11-05 04:38:12 +0100

    i dunno at least it didnt kill them lol

--- welcomebot.py
+++ welcomebot.py
@@ -1,8 +1,9 @@
-from mautrix.types import EventType
-from maubot import Plugin MessageEvent
+from mautrix.types import RoomID, EventType, MessageType
+from maubot import Plugin, MessageEvent
 from maubot.handlers import event
 
 class WelcomeBot(Plugin):
     @event.on(EventType.ROOM_MEMBER)
-    async def handler(self, event: MessageEvent) -> None:
-        event.send_message_event("testing")
+    async def send_welcome(self, evt: StateEvent) -> None:
+        if (evt.content.membership == Membership.JOIN):
+            self.client.send_message(evt.room_id, "FUCK")
